5c6fa1a4397a4edaae1e206a1f4af102f2de0f10
Lukas Zandomeneghi <lukaszandomeneghi@gmail.com>
2025-05-06 23:55:42 +0200

    I would have been already correct...

--- main.go
+++ main.go
@@ -7,7 +7,8 @@ func main() {
 	a.Initialize(
 		os.Getenv("APP_DB_USERNAME"),
 		os.Getenv("APP_DB_PASSWORD"),
-		os.Getenv("APP_DB_NAME"))
+		os.Getenv("APP_DB_NAME"),
+		os.Getenv("APP_DB_HOST"))
 
 	a.Run(":8080")
 }
--- main_test.go
+++ main_test.go
@@ -19,7 +19,8 @@ func TestMain(m *testing.M) {
 	a.Initialize(
 		os.Getenv("APP_DB_USERNAME"),
 		os.Getenv("APP_DB_PASSWORD"),
-		os.Getenv("APP_DB_NAME"))
+		os.Getenv("APP_DB_NAME"),
+		os.Getenv("APP_DB_HOST"))
 
 	ensureTableExists()
 	code := m.Run()
