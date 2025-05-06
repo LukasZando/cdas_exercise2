252655652eed53431153fccde2a72050a4d46982
Lukas Zandomeneghi <lukaszandomeneghi@gmail.com>
2025-05-07 00:13:39 +0200

    Bug fix

--- app.go
+++ app.go
@@ -33,7 +33,7 @@ func (a *App) Initialize(user, password, dbname, host string) {
 }
 
 func (a *App) Run(addr string) {
-	log.Fatal(http.ListenAndServe(":8010", a.Router))
+	log.Fatal(http.ListenAndServe(addr, a.Router))
 }
 
 func (a *App) getProduct(w http.ResponseWriter, r *http.Request) {
